26cbd41dd8bcaa1214d44ea0ba1b9ee3782de042
Craluminum2413 <beware.14b@gmail.com>
2022-10-19 00:30:06 +0300

    Allow to place playing card into domino board slot

--- src/Inventory/ItemSlotDominoBoard.cs
+++ src/Inventory/ItemSlotDominoBoard.cs
@@ -12,12 +12,12 @@ namespace TabletopGames
 
         public override bool CanTakeFrom(ItemSlot sourceSlot, EnumMergePriority priority = EnumMergePriority.AutoMerge)
         {
-            return sourceSlot.Itemstack.Collectible is ItemDominoPiece;
+            return sourceSlot.Itemstack.Collectible is ItemDominoPiece or ItemPlayingCard;
         }
 
         public override bool CanHold(ItemSlot sourceSlot)
         {
-            return sourceSlot.Itemstack.Collectible is ItemDominoPiece;
+            return sourceSlot.Itemstack.Collectible is ItemDominoPiece or ItemPlayingCard;
         }
     }
 }
