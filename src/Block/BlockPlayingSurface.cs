e59a0af67fcf7f49b91dbbe73599eab5317cba19
Craluminum2413 <beware.14b@gmail.com>
2022-11-08 11:32:19 +0200

    Do partial selection in PlayingSurface

--- src/Block/BlockPlayingSurface.cs
+++ src/Block/BlockPlayingSurface.cs
@@ -6,9 +6,7 @@ namespace TabletopGames
 {
     public class BlockPlayingSurface : Block
     {
-        public override bool DoParticalSelection(IWorldAccessor world, BlockPos pos) => false;
-        // public override string GetPlacedBlockName(IWorldAccessor world, BlockPos pos) => null;
-
+        public override bool DoParticalSelection(IWorldAccessor world, BlockPos pos) => true;
         public override Vec4f GetSelectionColor(ICoreClientAPI capi, BlockPos pos) => new(1, 1, 0, 1); // Yellow
 
         public override bool OnBlockInteractStart(IWorldAccessor world, IPlayer byPlayer, BlockSelection blockSel)
@@ -41,26 +39,5 @@ namespace TabletopGames
             blockStack.Attributes.SetInt("quantitySlots", blockEntity.quantitySlots);
             return blockStack;
         }
-
-        // public bool CreateSurface(IWorldAccessor world, BlockSelection blockSel, IPlayer player)
-        // {
-        //     if (!world.Claims.TryAccess(player, blockSel.Position, EnumBlockAccessFlags.BuildOrBreak))
-        //     {
-        //         player.InventoryManager.ActiveHotbarSlot.MarkDirty();
-        //         return false;
-        //     }
-
-        //     BlockPos pos;
-        //     if (blockSel.Face == null) pos = blockSel.Position;
-        //     else pos = blockSel.Position.AddCopy(blockSel.Face);
-
-        //     var belowBlock = world.BlockAccessor.GetBlock(pos.DownCopy());
-        //     if (!belowBlock.CanAttachBlockAt(world.BlockAccessor, this, pos.DownCopy(), BlockFacing.UP) && belowBlock != this) return false;
-
-        //     if (!player.Entity.Controls.CtrlKey) return false;
-
-        //     world.BlockAccessor.SetBlock(BlockId, pos);
-        //     return true;
-        // }
     }
 }
