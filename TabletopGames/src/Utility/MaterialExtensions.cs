371dcd31a048d8ee9f9872473d7863d7adc1b59a
Dana <beware.14b@gmail.com>
2024-12-25 19:18:31 +0200

    implement materials

---
+++ TabletopGames/src/Utility/MaterialExtensions.cs
@@ -0,0 +1,120 @@
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
+using Vintagestory.API.Common;
+using Vintagestory.API.Datastructures;
+using Vintagestory.ServerMods;
+
+namespace TabletopGames;
+
+public static class MaterialExtensions
+{
+    public static Dictionary<string, List<string>> GatherMaterials(this ICoreAPI api, RegistryObjectVariantGroup[] variantGroups)
+    {
+        Dictionary<string, List<string>> resolvedTypes = new();
+        foreach (RegistryObjectVariantGroup variantGroup in variantGroups)
+        {
+            List<string> types = new();
+
+            if (variantGroup?.States?.Any() == true)
+            {
+                types = types.Concat(variantGroup.States).ToList();
+            }
+
+            if (variantGroup?.LoadFromProperties != null)
+            {
+                IAsset asset = api.Assets.TryGet(variantGroup.LoadFromProperties.WithPathPrefixOnce("worldproperties/").WithPathAppendixOnce(".json"));
+                if (asset != null)
+                {
+                    IEnumerable<string> _types = (asset?.ToObject<StandardWorldProperty>()).Variants.Select((WorldPropertyVariant p) => p.Code.Path);
+                    types = types.Concat(_types).ToList();
+                }
+            }
+
+            if (!resolvedTypes.TryGetValue(variantGroup.Code, out List<string> _resolvedTypes))
+            {
+                resolvedTypes.Add(variantGroup.Code, types);
+            }
+            else
+            {
+                resolvedTypes[variantGroup.Code] = _resolvedTypes.Concat(types).ToList();
+            }
+        }
+        return resolvedTypes;
+    }
+
+    public static void FillCreativeInventory(this CollectibleObject obj, ICoreAPI api, Dictionary<string, List<string>> materials, params string[] tabs)
+    {
+        List<JsonItemStack> _stacks = new List<JsonItemStack>();
+
+        foreach (Dictionary<string, string> pairs in materials.GetCombinationsContainingAllKeys())
+        {
+            JsonObject _attributes = new JsonObject(new JObject());
+            _attributes.Token[Materials.AttributeName] = JToken.FromObject(new object());
+
+            foreach (KeyValuePair<string, string> pair in pairs)
+            {
+                _attributes.Token[Materials.AttributeName][pair.Key] = JToken.FromObject(pair.Value);
+            }
+
+            JsonItemStack _jstack = new JsonItemStack()
+            {
+                Code = obj.Code,
+                Type = obj.ItemClass,
+                Attributes = _attributes
+            };
+            _jstack.Resolve(api.World, obj.Code + " type");
+            _stacks.Add(_jstack);
+        }
+
+        obj.CreativeInventoryStacks = new CreativeTabAndStackList[]
+        {
+            new CreativeTabAndStackList() { Stacks = _stacks.ToArray(), Tabs = tabs }
+        };
+    }
+
+    public static List<Dictionary<string, string>> GetCombinationsContainingAllKeys(this Dictionary<string, List<string>> materials)
+    {
+        List<List<string>> combinations = GenerateCombinations(materials);
+        List<Dictionary<string, string>> finalResult = new List<Dictionary<string, string>>();
+
+        foreach (List<string> result in combinations)
+        {
+            finalResult.Add(new());
+
+            int materialIndex = 0;
+
+            foreach ((string material, _) in materials)
+            {
+                finalResult.Last().Add(material, result[materialIndex]);
+                materialIndex++;
+            }
+        }
+
+        return finalResult;
+    }
+
+    public static List<List<string>> GenerateCombinations(this Dictionary<string, List<string>> materials)
+    {
+        List<List<string>> results = new List<List<string>>();
+        CombineMaterials(materials, new List<string>(), new List<string>(materials.Keys), 0, results);
+        return results;
+    }
+
+    public static void CombineMaterials(Dictionary<string, List<string>> materials, List<string> current, List<string> keys, int index, List<List<string>> results)
+    {
+        if (index == keys.Count)
+        {
+            results.Add(new List<string>(current));
+            return;
+        }
+
+        string key = keys[index];
+        foreach (string item in materials[key])
+        {
+            current.Add(item);
+            CombineMaterials(materials, current, keys, index + 1, results);
+            current.RemoveAt(current.Count - 1);
+        }
+    }
+}
---
+++ TabletopGames/src/Utility/Materials.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Datastructures;
+
+namespace TabletopGames;
+
+public class Materials
+{
+    public const string AttributeName = "types";
+
+    protected Dictionary<string, string> Elements { get; set; } = new();
+
+    public int Count => Elements.Count;
+    public bool Any => Elements.Any();
+
+    public IOrderedEnumerable<Material> GetOrdered(string textureCode = null)
+    {
+        return Elements
+            .Select(x => new Material(x.Key, x.Value))
+            .OrderBy(x => x.Key);
+    }
+
+    public void GetDescription(StringBuilder dsc, List<string> langKeys, bool withDebugInfo = false)
+    {
+        if (!Elements.Any())
+        {
+            return;
+        }
+
+        if (langKeys != null)
+        {
+            foreach (string langKey in langKeys)
+            {
+                string newLangKey = ReplacePlaceholders(langKey);
+                dsc.AppendLine(Lang.GetMatching(newLangKey));
+            }
+        }
+
+        //dsc.AppendLine(Lang.GetMatching(material.ToString()));
+
+        if (!withDebugInfo)
+        {
+            return;
+        }
+
+        foreach (KeyValuePair<string, string> material in Elements)
+        {
+            if (withDebugInfo)
+            {
+                dsc.AppendLine($"({material.Key}-{material.Value})");
+            }
+        }
+    }
+
+    public static Materials FromTreeAttribute(ITreeAttribute rootTree)
+    {
+        Materials materials = new Materials();
+        if (!rootTree.HasAttribute(AttributeName) || !rootTree.GetTreeAttribute(AttributeName).Any())
+        {
+            return materials;
+        }
+
+        ITreeAttribute typesTree = rootTree.GetTreeAttribute(AttributeName);
+
+        foreach (string key in typesTree.Select(x => x.Key).Where(key => !materials.Elements.ContainsKey(key)))
+        {
+            materials.Elements.Add(key, typesTree.GetString(key));
+        }
+
+        return materials;
+    }
+
+    public void ToTreeAttribute(ITreeAttribute rootTree)
+    {
+        foreach ((string key, string val) in Elements)
+        {
+            rootTree.GetOrAddTreeAttribute(AttributeName).SetString(key, val);
+        }
+    }
+
+    public static Materials FromStack(ItemStack stack)
+    {
+        return FromTreeAttribute(stack.Attributes);
+    }
+
+    public void ToStack(ItemStack stack)
+    {
+        ToTreeAttribute(stack.Attributes);
+    }
+
+    public string ReplacePlaceholders(string input)
+    {
+        foreach ((string key, string value) in Elements)
+        {
+            input = input.Replace("{" + key + "}", value);
+        }
+        return input;
+    }
+
+    public override string ToString()
+    {
+        StringBuilder result = new StringBuilder();
+        if (Elements.Any())
+        {
+            result.Append(string.Join('-', Elements.Select(x => $"{x.Key}-{x.Value}")));
+        }
+        return result.ToString();
+    }
+}
+
+public class Material
+{
+    public string Key { get; protected set; }
+    public string Value { get; protected set; }
+
+    public Material(string key, string value)
+    {
+        Key = key;
+        Value = value;
+    }
+
+    public static Material FromString(string keyVal)
+    {
+        string[] list = keyVal.Split('-');
+        if (list.Length != 2)
+        {
+            // TODO throw errow
+            return null;
+        }
+        return new Material(list[0], list[1]);
+    }
+
+    public override string ToString()
+    {
+        return $"{Key}-{Value}";
+    }
+}
