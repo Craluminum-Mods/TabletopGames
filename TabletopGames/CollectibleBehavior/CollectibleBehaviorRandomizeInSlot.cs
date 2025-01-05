748e4eefd99094506256be871860018fba0b3cdb
Dana <beware.14b@gmail.com>
2025-01-05 15:40:44 +0200

    add support for variants for randomizeinslot behavior

--- TabletopGames/CollectibleBehavior/CollectibleBehaviorRandomizeInSlot.cs
+++ TabletopGames/CollectibleBehavior/CollectibleBehaviorRandomizeInSlot.cs
@@ -8,16 +8,17 @@ namespace TabletopGames;
 public class CollectibleBehaviorRandomizeInSlot : CollectibleBehavior
 {
     private ICoreAPI api;
-    private string targetVariant;
-    private List<string> possibleVariantValues = new();
+
+    private Dictionary<string, string> targetVariantByType;
+    private Dictionary<string, List<string>> possibleVariantValuesByType = new();
 
     public CollectibleBehaviorRandomizeInSlot(CollectibleObject collObj) : base(collObj) { }
 
     public override void Initialize(JsonObject properties)
     {
         base.Initialize(properties);
-        targetVariant = properties["targetVariant"].AsString();
-        possibleVariantValues = properties["possibleVariantValues"].AsObject(new List<string>());
+        targetVariantByType = properties["targetVariant"].AsObject<Dictionary<string, string>>();
+        possibleVariantValuesByType = properties["possibleVariantValues"].AsObject(new Dictionary<string, List<string>>());
     }
 
     public override void OnLoaded(ICoreAPI api)
@@ -27,11 +28,17 @@ public class CollectibleBehaviorRandomizeInSlot : CollectibleBehavior
 
     public void RandomizeAttributes(ItemStack stack)
     {
+        Variants variants = Variants.FromStack(stack);
+        if (!variants.FindByVariant(targetVariantByType, out string targetVariant) || !variants.FindByVariant(possibleVariantValuesByType, out List<string> possibleVariantValues))
+        {
+            return;
+        }
+
         if (possibleVariantValues == null || !possibleVariantValues.Any() || string.IsNullOrEmpty(targetVariant))
         {
             return;
         }
-        Variants variants = Variants.FromStack(stack);
+
         variants.Set(targetVariant, possibleVariantValues[api.World.Rand.Next(possibleVariantValues.Count)]);
         variants.ToStack(stack);
     }
