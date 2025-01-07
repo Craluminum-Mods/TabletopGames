7477701f8e750067091c597768906a5a4d408028
Dana <beware.14b@gmail.com>
2025-01-07 22:39:42 +0200

    added dice animation

--- TabletopGames/Item/ItemDice.cs
+++ TabletopGames/Item/ItemDice.cs
@@ -1,9 +1,21 @@
-﻿using Vintagestory.API.Common;
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
 
 namespace TabletopGames;
 
+/// <summary>
+/// Represents a dice item that handles animations and randomization behavior when dropped
+/// </summary>
 public class ItemDice : ItemShapeTexturesFromAttributes
 {
+    public ModelTransform OriginalGroundTransform;
+
+    public override void OnLoaded(ICoreAPI api)
+    {
+        base.OnLoaded(api);
+        OriginalGroundTransform = GroundTransform.Clone();
+    }
+
     public override void OnGroundIdle(EntityItem entityItem)
     {
         CollectibleBehaviorRandomizeInSlot behaviorRandomizeInSlot = GetBehavior<CollectibleBehaviorRandomizeInSlot>();
@@ -14,5 +26,37 @@ public class ItemDice : ItemShapeTexturesFromAttributes
         }
 
         base.OnGroundIdle(entityItem);
+    }
+
+    public override void OnBeforeRender(ICoreClientAPI capi, ItemStack itemstack, EnumItemRenderTarget target, ref ItemRenderInfo renderinfo)
+    {
+        if (target == EnumItemRenderTarget.Ground && renderinfo.InSlot is EntityItemSlot slot)
+        {
+            int ticks = itemstack.TempAttributes.GetAsInt("tabletopGames.ticksUntilStopRolling");
+            if (ticks < 350)
+            {
+                renderinfo.Transform.Rotation.X = capi.World.ElapsedMilliseconds * 6;
+                renderinfo.Transform.Rotation.Y = capi.World.ElapsedMilliseconds * 6;
+                renderinfo.Transform.Rotation.Z = capi.World.ElapsedMilliseconds * 6;
+                itemstack.TempAttributes.SetInt("tabletopGames.ticksUntilStopRolling", ticks + 1);
+            }
+            else
+            {
+                if (!IsSameTransform(renderinfo.Transform, OriginalGroundTransform))
+                {
+                    renderinfo.Transform = OriginalGroundTransform.Clone();
+                }
+            }
+        }
+
+        base.OnBeforeRender(capi, itemstack, target, ref renderinfo);
+    }
+
+    public bool IsSameTransform(ModelTransform transform1, ModelTransform transform2)
+    {
+        return transform1.Rotation == transform2.Rotation
+            && transform1.Translation == transform2.Translation
+            && transform1.Origin == transform2.Origin
+            && transform1.ScaleXYZ == transform2.ScaleXYZ;
     }
 }
