c2083d6d0c4307474e107099400a3ccd8d3aa237
Dana <beware.14b@gmail.com>
2025-02-14 15:22:27 +0200

    minor tweak

--- TabletopGames/Inventory/ItemSlotTabletop.cs
+++ TabletopGames/Inventory/ItemSlotTabletop.cs
@@ -5,23 +5,23 @@ namespace TabletopGames;
 public class ItemSlotTabletop : ItemSlot
 {
     public EnumSlotType SlotType { get; }
-    public TabletopTags TabletopTags { get; }
+    public TabletopTags BoardTags { get; }
 
-    public ItemSlotTabletop(InventoryBase inventory, TabletopTags tabletopTags, EnumSlotType slotType) : base(inventory)
+    public ItemSlotTabletop(InventoryBase inventory, TabletopTags boardTags, EnumSlotType slotType) : base(inventory)
     {
         SlotType = slotType;
-        TabletopTags = tabletopTags;
+        BoardTags = boardTags;
     }
 
     public override int MaxSlotStackSize => 1;
 
     public override bool CanHold(ItemSlot sourceSlot)
     {
-        return TabletopTags.AreTagsCompatible(TabletopTags, stack: sourceSlot?.Itemstack) && base.CanHold(sourceSlot);
+        return TabletopTags.AreTagsCompatible(BoardTags, stack: sourceSlot?.Itemstack) && base.CanHold(sourceSlot);
     }
 
     public override bool CanTakeFrom(ItemSlot sourceSlot, EnumMergePriority priority = EnumMergePriority.AutoMerge)
     {
-        return TabletopTags.AreTagsCompatible(TabletopTags, stack: sourceSlot?.Itemstack) && base.CanTakeFrom(sourceSlot, priority);
+        return TabletopTags.AreTagsCompatible(BoardTags, stack: sourceSlot?.Itemstack) && base.CanTakeFrom(sourceSlot, priority);
     }
 }
