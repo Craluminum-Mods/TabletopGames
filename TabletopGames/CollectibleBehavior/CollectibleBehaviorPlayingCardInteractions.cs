aa0d6a6dd8a2239405c5107c6f4948ae3e8ceced
Dana <beware.14b@gmail.com>
2025-03-23 21:53:57 +0200

    add combine cards tooltip to cards

--- TabletopGames/CollectibleBehavior/CollectibleBehaviorPlayingCardInteractions.cs
+++ TabletopGames/CollectibleBehavior/CollectibleBehaviorPlayingCardInteractions.cs
@@ -19,6 +19,11 @@ public class CollectibleBehaviorPlayingCardInteractions : CollectibleBehavior, I
             {
                 ActionLangCode = "tabletopgames:heldhelp-shuffle",
                 HotKeyCode = "tabletopgames:shuffle"
+            },
+            new WorldInteraction()
+            {
+                ActionLangCode = "tabletopgames:heldhelp-combinecards",
+                HotKeyCode = "tabletopgames:combinecards"
             }
         };
 
